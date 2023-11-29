ac00775dd9b7e220be7f3e46796107a97637d698
joao clodoaldo <joao.clodo1@gmail.com>
2023-11-29 19:50:57 -0300

    alteracao da versao

---
+++ src/test/java/br/com/unidep/demoprova/DemoProvaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.unidep.demoprova;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoProvaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
